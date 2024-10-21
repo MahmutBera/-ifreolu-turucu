7800d529f95aba1d8bf0ca31838908f1f39f2269
MahmutBera <mahmutberacoban1@gmail.com>
2024-10-21 20:46:33 +0300

    Create şifreoluşturucu.py

---
+++ şifreoluşturucu.py
@@ -0,0 +1,10 @@
+
+import random
+
+karakterler = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+uzunluk = int(input("şifreniz kaç karakter olsun."))
+parola = ""
+for i in range(uzunluk):
+    karakter = random.choice(karakterler)
+    parola += karakter
+print("parolanız:",parola)
